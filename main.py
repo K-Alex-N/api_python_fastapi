a6914a082ebd3613556c142f919e72bca726362e
aleksei <KurochkinAlexei@yandex.ru>
2025-05-25 17:21:33 +0200

    seems it works

--- main.py
+++ main.py
@@ -1,5 +1,37 @@
-﻿from fastapi import FastAPI
+﻿import uvicorn
+from fastapi import FastAPI
+
+from tests.test_fastapi import spending1
+
+app = FastAPI()
+
+spendings = [
+    ["мороженка", "100", "Дин"],
+    ["Аренда квартиры", "330", "Евро"],
+    ["Годовая подписка Яндекс", "2990", "Руб"]
+]
 
 
-print("adsad")
+@app.get("/balance")
+def get_balance():
 
+
+
+# def add_expense():
+# Date
+# Amount
+# Description
+# currency
+
+# def add_income():
+# Delete
+
+# def modify_expense():
+
+# Recurring Expense
+# One-time Expense
+# аренда квартиры
+
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", reload=True)
--- tests/test_fastapi.py
+++ tests/test_fastapi.py
@@ -5,3 +5,8 @@ from httpx import AsyncClient, ASGITransport
 async def test_get():
     ...
 
+
+
+spending1 = ["мороженка", "100", "Дин"]
+spending2 = ["Аренда квартиры", "330", "Евро"]
+spending3 = ["Годовая подписка Яндекс", "2990", "Руб"]
