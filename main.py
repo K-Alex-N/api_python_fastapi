e2150414cbd1c94d1e20f7d295a35c8a63640633
aleksei <KurochkinAlexei@yandex.ru>
2025-05-25 22:23:52 +0200

    seems it works

--- main.py
+++ main.py
@@ -1,37 +1,69 @@
 ﻿import uvicorn
-from fastapi import FastAPI
-
-from tests.test_fastapi import spending1
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
 
 app = FastAPI()
 
-spendings = [
-    ["мороженка", "100", "Дин"],
-    ["Аренда квартиры", "330", "Евро"],
-    ["Годовая подписка Яндекс", "2990", "Руб"]
+spending = [
+    {
+        "id": 1,
+        "description": "мороженка",
+        "amount": 100,
+        "currency": "Дин"
+    },
+    {
+        "id": 2,
+        "description": "Аренда квартиры",
+        "amount": 330,
+        "currency": "Евро"
+    },
+    {
+        "id": 3,
+        "description": "Годовая подписка Яндекс",
+        "amount": 2990,
+        "currency": "Руб"
+    }
 ]
 
 
 @app.get("/balance")
 def get_balance():
+    return spending
 
 
+@app.get("/{id}")
+def get_spending(id: int):
+    for s in spending:
+        if s["id"] == id:
+            return s
+    raise HTTPException(status_code=404, detail="Not found")
 
-# def add_expense():
-# Date
-# Amount
-# Description
-# currency
+
+class Expense(BaseModel):
+    description: str
+    amount: int
+    currency: str
+
+
+@app.post("/")
+def add_expense(expense: Expense):
+    spending.append({
+        "id": len(spending) + 1,
+        "description": expense.description,
+        "amount": expense.amount,
+        "currency": expense.currency
+    })
+    return {"success": True, "message": "Expense added"}
+
 
 # def add_income():
 # Delete
 
 # def modify_expense():
 
-# Recurring Expense
-# One-time Expense
-# аренда квартиры
-
+# improvements
+# date (auto + modification)
+# Expense One-time + Recurring
 
 if __name__ == "__main__":
     uvicorn.run("main:app", reload=True)
