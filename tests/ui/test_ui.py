1fce2bc4bb6829e1b4b457dbb0d5eebe2493e044
Alexey Kurochkin <KurochkinAlexei@yandex.ru>
2025-06-12 09:00:13 +0200

    .

--- tests/ui/test_ui.py
+++ tests/ui/test_ui.py
@@ -1,8 +1,11 @@
 import asyncio
+import os
+
+import pytest
 from playwright.async_api import Playwright, async_playwright, expect
 
-headless = True
-slow_mo = 0
+HEADLESS = os.getenv("HEADLESS", False)
+SLOW_MO = os.getenv("SLOW_MO", 200)
 
 VALID_USERNAME = "standard_user"
 VALID_PASSWORD = "secret_sauce"
@@ -10,66 +13,56 @@ INVALID_USERNAME = "asd"
 INVALID_PASSWORD = "asd"
 
 
+@pytest.mark.asyncio
 async def test_user_successfully_logs_in_with_valid_credentials():
     async with async_playwright() as playwright:
-        browser = await playwright.chromium.launch(headless=headless, slow_mo=slow_mo)
+        browser = await playwright.chromium.launch(headless=HEADLESS, slow_mo=SLOW_MO)
         page = await browser.new_page()
 
-
+        # мб проставить аллюр метки --- "Открываем ..."
+        await page.goto("https://www.saucedemo.com/")
 
         # locators
-        # pas --- #user-name    standard_user
-        # pas --- #password     secret_sauce
-        # but -  #login-button
+        # pas ---
+        # pas --
+        # but -
         # если не верный логин или пароль    .error-message-container
 
-        # мб проставить аллюр метки --- "Открываем Google.com..."
-        await page.goto("https://www.saucedemo.com/")
-        # await page.wait_for_load_state("networkidle")  # Ждем, пока сеть не будет неактивна. зачем это?????
+        print("Ввожу username...")
+        await page.fill("#user-name", "standard_user")
+        await page.fill("#password", "secret_sauce")
+        await page.click("#login-button")
 
-        # закрываем куки. А что если их нет????!!!
-        await page.click(".QS5gu.sy4vM")
-
-        print("Ввожу запрос в поиск...")
-        await page.fill('#APjFqb', "youtube")
-        # Или await page.get_by_label("Поиск").fill("Playwright Python example")
-
-        print("Нажимаю кнопку поиска...")
-        await page.click('input[name="btnK"]')
-
-        # что делать если появиться проверка что я робот
+        await expect(page).to_have_url("https://www.saucedemo.com/inventory.html")
 
         # 6. Ожидание загрузки страницы результатов
         # Можно ждать конкретного элемента на странице результатов
-        await page.wait_for_selector('#search')  # Ждем появления блока с результатами поиска
-        print(f"Заголовок страницы результатов: {await page.title()}")
+        # await page.wait_for_selector('#search')  # Ждем появления блока с результатами поиска
+        # print(f"Заголовок страницы результатов: {await page.title()}")
 
         # 7. Извлечение заголовков первых нескольких результатов поиска
-        print("\nЗаголовки первых 5 результатов поиска:")
+        # print("\nЗаголовки первых 5 результатов поиска:")
         # Ищем все элементы с классом 'LC20lb MBeuO DCoPu' (это заголовки результатов)
         # locator() возвращает Locator, который может найти несколько элементов
         # all_text_contents() извлекает текст из всех найденных элементов
-        search_results_titles = await page.locator('h3').all_text_contents()
-        for i, title in enumerate(search_results_titles[:5]):  # Берем первые 5
-            print(f"{i + 1}. {title}")
+        # search_results_titles = await page.locator('h3').all_text_contents()
+        # for i, title in enumerate(search_results_titles[:5]):  # Берем первые 5
+        #     print(f"{i + 1}. {title}")
 
         # 8. Закрытие браузера
         print("\nЗакрываю браузер.")
         await browser.close()
 
+        # .app_logo
 
         # Лучше сделать LOGOUT на следуюшей странице
 
 
+@pytest.mark.asyncio
 async def test_user_can_not_logs_in_with_invalid_username():
     pass
 
 
+@pytest.mark.asyncio
 async def test_user_can_not_logs_in_with_invalid_password():
     pass
-
-
-if __name__ == "__main__":
-    headless = False
-    slow_mo = 200
-    asyncio.run(test_user_successfully_logs_in_with_valid_credentials(headless=headless, slow_mo=slow_mo))
