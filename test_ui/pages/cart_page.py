8c23d7f94b38b95c79c6c06b4e1bfbfc0e15516e
aleksei <KurochkinAlexei@yandex.ru>
2025-06-24 21:09:40 +0200

    clean up

--- test_ui/pages/cart_page.py
+++
@@ -1,8 +0,0 @@
-﻿from .base_page import BasePage
-
-class CartPage(BasePage):
-    URL = "https://www.saucedemo.com/cart.html"
-    CART_ITEMS = ".cart_item"
-
-    async def get_cart_items_count(self) -> int:
-        return await self.page.locator(self.CART_ITEMS).count()
