59994c2fb97f6cb62adbb5b0fc8ef8dbe7d9ef70
aleksei <KurochkinAlexei@yandex.ru>
2025-07-10 22:08:27 +0200

    elastic stack

---
+++ app/api/logger_config.py
@@ -0,0 +1,33 @@
+﻿import logging
+import os
+import json
+from datetime import datetime
+
+log_dir = "logs"
+os.makedirs(log_dir, exist_ok=True)
+
+class JsonFormatter(logging.Formatter):
+    def format(self, record):
+        log_record = {
+            "@timestamp": datetime.utcnow().isoformat(),
+            "level": record.levelname,
+            "message": record.getMessage(),
+            "logger": record.name,
+            "module": record.module,
+        }
+
+        if hasattr(record, "extra"):
+            log_record.update(record.extra)
+
+        return json.dumps(log_record)
+
+def setup_logger():
+    logger = logging.getLogger("fastapi_logger")
+    logger.setLevel(logging.INFO)
+
+    if not logger.handlers:
+        handler = logging.FileHandler(os.path.join(log_dir, "app.log"))
+        handler.setFormatter(JsonFormatter())
+        logger.addHandler(handler)
+
+    return logger
--- app/api/main.py
+++ app/api/main.py
@@ -98,22 +98,22 @@ import uvicorn
 
 # ---------------------------------------------------------
 
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException, Request, Response
 from prometheus_fastapi_instrumentator import Instrumentator
 from prometheus_client import Counter, Gauge
+import string
 
+from .logger_config import setup_logger
 from .schemas import Transaction, HealthCheckResponse
 from .crud_async import add_random_transaction_async, get_random_transaction_async
 from .crud_sync import add_random_transaction_sync, get_random_transaction_sync
 
+logger = setup_logger()
 app = FastAPI()
-Instrumentator().instrument(app).expose(app)
+
 
 # --- Healthcheck ---
 
-# @app.get("/")
-# def healthcheck():
-#     return {"status": "ok"}
 
 from .database import async_db, sync_db
 from pymongo.errors import PyMongoError
@@ -161,8 +161,65 @@ def read_sync_transaction():
         raise HTTPException(status_code=404, detail="No transactions found")
     return transaction
 
+# ---------------for elastic------------------------------------------
+
+@app.middleware("http")
+async def log_requests(request: Request, call_next):
+    ip = request.client.host
+    endpoint = request.url.path
+    method = request.method
+
+    response: Response = await call_next(request)
+    status_code = response.status_code
+
+    logger.info(
+        f"{method} {endpoint} - {status_code}",
+        extra={
+            "extra": {
+                "ip": ip,
+                "endpoint": endpoint,
+                "method": method,
+                "status": status_code,
+            }
+        }
+    )
+
+    return response
+
+
+
+@app.get("/generate-random-logs")
+def generate_random_logs():
+    levels = ["info", "debug", "warning", "error", "supper_error"]
+    actions = ["login", "logout", "purchase", "view", "click"]
+    countries = ["US", "DE", "FR", "IN", "RU", "CN", "BR"]
+    devices = ["mobile", "desktop", "tablet", "smart_tv"]
+
+    for _ in range(10):
+        level = random.choice(levels)
+        log_data = {
+            "user_id": random.randint(1000, 9999),
+            "action": random.choice(actions),
+            "duration": round(random.uniform(0.1, 5.0), 2),
+            "success": random.choice([True, False]),
+            "country": random.choice(countries),
+            "device": random.choice(devices),
+            "session_id": ''.join(random.choices(string.ascii_letters + string.digits, k=12))
+        }
+
+        msg = f"User {log_data['user_id']} performed {log_data['action']}"
+
+        # Логируем на нужном уровне
+        getattr(logger, level)(
+            msg,
+            extra={"extra": log_data}
+        )
+
+    return {"message": "Random logs generated"}
+
 # ---------------for prometheus------------------------------------------
 
+Instrumentator().instrument(app).expose(app)
 
 ERROR_COUNTER = Counter("fastapi_errors_total", "Total number of simulated errors")
 ACTIVE_USERS = Gauge("fastapi_active_users", "Number of active users")
