9c81ea70b9dd28edb76d899b8359b017092c9a70
Alexey Kurochkin <KurochkinAlexei@yandex.ru>
2025-08-05 14:13:01 +0200

    refactoring

--- tests/api/services/categories/urls.py
+++ tests/api/services/categories/urls.py
@@ -7,10 +7,6 @@ class URLs:
     create_category = CATEGORIES_URL
     get_all_categories = CATEGORIES_URL
 
-    # get_category_by_id = lambda self, _id: f"{self.CATEGORIES_URL}{_id}"
-    # update_category = lambda self, _id: f"{self.CATEGORIES_URL}{_id}"
-    # delete_category = lambda self, _id: f"{self.CATEGORIES_URL}{_id}"
-
     def get_category_by_id(self, _id):
         return f"{self.CATEGORIES_URL}{_id}"
 
