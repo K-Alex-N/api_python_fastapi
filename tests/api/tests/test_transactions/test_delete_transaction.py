56537dfc21d9d714598706ae398ad51b6af4d73c
aleksei <KurochkinAlexei@yandex.ru>
2025-11-26 09:07:42 +0100

    refactoring

--- tests/api/tests/test_transactions/test_delete_transaction.py
+++ tests/api/tests/test_transactions/test_delete_transaction.py
@@ -18,11 +18,15 @@ class TestDeleteTransaction:
         create_transaction = CreateTransaction(client)
         delete_transaction = DeleteTransaction(client)
 
+        # create a transactions
         await create_transaction.create_transaction(await payloads.create_transaction())
         transaction_id = create_transaction.response_json.get("id")
         assert await create_transaction.check_response_is(HTTPStatus.OK)
 
+        # delete the transactions
         await delete_transaction.delete_transaction(transaction_id)
+
+        # check the transactions was deleted
         assert await delete_transaction.check_response_is(HTTPStatus.OK)
         assert await delete_transaction.is_transaction_deleted(transaction_id)
 
@@ -33,10 +37,10 @@ class TestDeleteTransaction:
             12345,
         ],
     )
-    async def test_delete_transaction_fails(self, client, transaction_id) -> None:
+    async def test_delete_transaction_fails_with_wrong_transaction_id(
+        self, client, transaction_id
+    ) -> None:
         delete_transaction = DeleteTransaction(client)
         await delete_transaction.delete_transaction(transaction_id)
 
-        assert await delete_transaction.check_response_is(
-            HTTPStatus.UNPROCESSABLE_ENTITY
-        )
+        assert await delete_transaction.check_response_is(HTTPStatus.UNPROCESSABLE_ENTITY)
